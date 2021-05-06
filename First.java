1f107f9b6689b06690e0239e6d267cd3388f0411
KeerthiVibisan-2211 <83780331+KeerthiVibisan-2211@users.noreply.github.com>
2021-05-06 23:14:35 +0530

    Created First.java
    My First MyCaptain JAVA project, to know the working on Git Hub.

---
+++ First.java
@@ -0,0 +1,10 @@
+//My First MyCaptain JAVA project.
+public class First 
+{
+	public static void main(String args[])
+	{
+		System.out.println("Iam KEERTHI VIBISAN S");
+		System.out.println("Hello");
+		System.out.print("Thankyou");
+	}
+}
