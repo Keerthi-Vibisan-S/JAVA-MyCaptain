4fdfebd9f8c6c051a52f715eb927b7b382918e37
KeerthiVibisan-2211 <83780331+KeerthiVibisan-2211@users.noreply.github.com>
2021-05-12 23:04:06 +0530

    Created Second.java
    My second MyCaptain java project.

--- Second.java
+++ Second.java
@@ -1,8 +1,8 @@
 //My second java prg
 // To convert minutes into years and days using methods.
 public class Main {
-	int mins,hours,days,years,Remaining_Days;
-	void calculate(int minutes)
+	static int mins,hours,days,years,Remaining_Days;
+	static void calculate(int minutes)
 	{
 		mins=minutes;
 		hours = mins/60;
@@ -11,15 +11,14 @@ public class Main {
 
 		Remaining_Days = days % 365;
 	}
-	void display()
+	static void display()
 	{
 		System.out.println(mins+" minutes is approximately "+years+" years "+Remaining_Days+" days");
 	}
 
 	public static void main(String[] args) {
-		Main obj=new Main();
 		int minutes=3456789;
-		obj.calculate(minutes);
-		obj.display();
+		calculate(minutes);
+		display();
 		}
 }
